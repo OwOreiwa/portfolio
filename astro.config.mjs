598e300ae7f523416d37f83acc0b1a3e723c68fa
4070-TI-SUPER\owore <oworeiwa>
2026-06-09 01:32:40 +0900

    to gh-pages branch

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,4 +9,24 @@ export default defineConfig({
 	site: 'https://OwOreiwa.github.io',
 	base: '/personal',
 	integrations: [mdx(), sitemap()],
+	build: {
+    inlineStylesheets: 'never',
+		assets: 'assets',
+  },
+	vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          assetFileNames: (assetInfo) => {
+            let extType = assetInfo.names[0].split('.').at(-1);
+            if (extType && /png|jpe?g|svg|gif|webp|ico/i.test(extType)) extType = 'img';
+            if (extType && /css|scss/i.test(extType)) extType = 'css';
+            return `assets/${extType}/[name][extname]`;
+          },
+        },
+      },
+			assetsInlineLimit: 0,
+			cssCodeSplit: false,
+    },
+  },
 });
