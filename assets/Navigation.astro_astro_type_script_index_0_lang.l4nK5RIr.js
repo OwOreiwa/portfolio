5a937e6c8d81725a8acf200c674c03691a5beb3a
4070-TI-SUPER\owore <oworeiwa>
2026-07-02 00:46:08 +0900

    Updates

---
+++ assets/Navigation.astro_astro_type_script_index_0_lang.l4nK5RIr.js
@@ -0,0 +1,1 @@
+document.querySelector(".hamburger")?.addEventListener("click",()=>{document.querySelector(".menu")?.classList.toggle("open"),document.querySelector(".hamburger")?.classList.toggle("open")});
